3c731598b54f0bc4d0d2e95f692b46c082415dd9
debzow <david.gerard.42@gmail.com>
2019-02-10 19:13:25 +0100

    flash and layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery3
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function() {
+  $(".alert").fadeOut(4000);
+});
--- test/controllers/meals_controller_test.rb
+++ test/controllers/meals_controller_test.rb
@@ -1,14 +1,27 @@
 require 'test_helper'
 
 class MealsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get meals_index_url
-    assert_response :success
+
+  def setup
+    @michael = User.create(first_name: "Michael",last_name: "Gerard",email: "michael@test.fr", password: 'topsecret', password_confirmation: 'topsecret')
+    @meal_type = MealType.create(name: "vegan")
+    @meal = Meal.create(name: "Tarte", description: "Delicieux plat", image_url: "", meal_type_id:  @meal_type.id)
   end
 
-  test "should get show" do
-    get meals_show_url
-    assert_response :success
+  test "access meals#shwo while sign-in" do
+    #sign-in
+    get user_session_path
+    post user_session_path, params:{user:{email:@michael.email, password:"topsecret", remember_me:"0"}}
+    #access meals#shwo
+    get meals_show_path(id: @meal.id)
+    assert_response(:success)
   end
 
+  test "no access meals#shwo while not sign-in" do
+    get meals_show_path(id: @meal.id)
+    assert_redirected_to(root_path)
+    follow_redirect!
+    #flash message
+    assert flash[:alert]
+  end
 end
